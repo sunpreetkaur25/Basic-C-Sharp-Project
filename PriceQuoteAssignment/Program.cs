64831d0e6394b10fe2acc1274e48778ae7f02178
sunpreetkaur25 <Sunpreet.kaur2588@gmail.com>
2021-11-16 21:07:19 -0500

    Loop Assignment

---
+++ PriceQuoteAssignment/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace PriceQuote
+    {
+        internal class Program
+        {
+            static void Main()
+            {
+                string A = "Welcome to Package Express. Please follow the instructions below.";
+                Console.WriteLine(A);
+
+                Console.WriteLine("What is the package weight?");
+                int weight = Convert.ToInt32(Console.ReadLine());
+                if (weight> 50)
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                else
+                Console.WriteLine("What is the package width?");
+                int width = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("What is the package height");
+                int height = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("What is the package length?");
+                int length = Convert.ToInt32(Console.ReadLine());
+
+            int dimension = width + height + length;
+            if (dimension > 50)
+                Console.WriteLine("Package too big to be shipped via Package Express.");
+         
+            int B = width * height * length;
+            int C = B * weight;
+            int D = C/100;
+
+            Console.WriteLine("Your estimated total for shipping this package is:" + C + "$");
+            Console.ReadLine();
+            }
+        }
+    }
--- while_and_dowhile/Program.cs
+++ while_and_dowhile/Program.cs
@@ -1,15 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace while_and_dowhile
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            int i = 0;
+            while (i < 5)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+            int j = 0;
+            do
+            {
+                Console.WriteLine(j);
+                j++;
+            }
+            while (j < 5);
+            Console.ReadLine();
+        }
         }
     }
-}
