f559f5acdac795cdc51479eeeab3b8d732498f38
sunpreetkaur25 <Sunpreet.kaur2588@gmail.com>
2021-11-16 20:21:57 -0500

    Boolean Assignment

---
+++ BooleanAssignment.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace PriceQuote
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string A = "Anonymous Income Comparison Program";
+            Console.WriteLine(A);
+            Console.WriteLine("Person 1");
+
+            Console.WriteLine("What is your Age?");
+            int Age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI?");
+            bool dui = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("How many speeding tickets do you have?");
+            int speedingTickets = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Qualified?");
+            bool qualified = (Age > 15 && dui == false && speedingTickets <= 3);
+            Console.WriteLine(qualified);
+            Console.ReadLine();
+        }
+    }
+}
