bf58007a3ae301df5b2f39f2c7d6c31c4274295a
sunpreetkaur25 <Sunpreet.kaur2588@gmail.com>
2021-12-04 15:38:30 -0500

    Assignment6_strings

---
+++ Array/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<string> intlist = new List<string>();
+            intlist.Add("My");
+            intlist.Add("name");
+            intlist.Add("is");
+            intlist.Add("Sunpreet");
+            intlist.Add("Kaur");
+            Console.WriteLine();
+            Console.ReadLine();
+
+            int[] numArray = new int[5];
+            numArray[0] = 1;
+            numArray[1] = 2;    
+            numArray[2] = 3;
+            numArray[3] = 4;
+            numArray[4] = 5;
+
+            int[] numArray1 = new int[5] {};
+
+            Console.WriteLine(numArray1);
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ Assignment6_strings/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Assignment6_strings
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+            int j = 0;
+            int x = 0;
+            List<string> names = new List<string>() { "Mike", "Don", "Mark", "Paul", "Rita", "Mike", "Don" };
+            foreach (string name in names)
+            { x = 0;
+                       
+                for (int i = 0; i < j;i++) 
+                {
+                  
+                    if (name == names[i]) 
+                    {
+                        x = 1;
+                        Console.WriteLine(names[i] + " has already appeared in the list");
+                    }
+                }
+                if (x == 0) 
+                { Console.WriteLine(name + " has not already appeared in the list"); }
+
+                j++;
+            }
+                    {
+                Console.WriteLine(names);
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
