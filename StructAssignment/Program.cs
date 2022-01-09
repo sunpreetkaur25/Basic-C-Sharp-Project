ca543e69b9386273071d34eba7a88646bf5cbf7c
sunpreetkaur25 <Sunpreet.kaur2588@gmail.com>
2022-01-08 20:29:47 -0500

    StructAssignment

---
+++ StructAssignment/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;  
+struct Number
+{  
+    public Decimal Amount;    
+}  
+class x {  
+    public static void Main() {  
+        Number ms = new Number();  
+         
+        
+        Console.WriteLine("The Amount is", ms);  
+    }  
+}   
