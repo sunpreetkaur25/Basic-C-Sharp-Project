27460f69dabd6db8211aa2aaab54fbee8c6eed33
sunpreetkaur25 <Sunpreet.kaur2588@gmail.com>
2022-01-08 19:26:37 -0500

    EnumAssignment

---
+++ EnumAssignment/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace EnumAssignment
+{
+    internal class Program
+    {
+       
+          enum weekdays
+        {
+            Sunday,    
+            Monday,   
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday      
+        }
+
+        static void Main(string[] args)
+        {
+        try {
+                 Console.ReadLine();
+                Console.WriteLine("enter the current day of the week");
+
+                weekdays myVar = weekdays.Monday;
+                Console.WriteLine(myVar);
+            }
+        catch (Exception e)
+{
+  Console.WriteLine("Please enter an actual day of the week");
+}
+}
+    }
+}
