49dac27145cefab1abc2ea4e7874cb43a880df30
sunpreetkaur25 <Sunpreet.kaur2588@gmail.com>
2022-01-21 20:43:52 -0500

    Exception handling assignment

---
+++ Additional Features Assignment/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Additional_Features_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { }
+            class C
+        {
+            public const int var = 12;
+        }
+    }
+    }
+
---
+++ Exception Handling Assignment/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_Handling_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter the Age in the format dd-mm-yyyy");
+                DateTime dt = Convert.ToDateTime(Console.ReadLine());
+
+                int year = dt.Year;
+
+                Console.WriteLine("The year in the date you entered: " + year);
+            }
+            catch
+            {
+                Console.WriteLine("Please enter a positive or non-zero number");
+            }
+            catch
+            {
+                Console.WriteLine("Please enter a valid number");
+            }
+            Console.ReadLine();
+        }
+    }
+    }
