a0c68c4077defa4d1e595ebc8387778a263c01c9
sunpreetkaur25 <Sunpreet.kaur2588@gmail.com>
2022-01-21 19:35:41 -0500

    assignment

---
+++ Datetime Assignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+namespace DateTime
+{
+    class DateTime
+    {
+        private static DateTime now;
+        public static void Main()
+
+        {
+            DateTime now = DateTime.now;
+            Console.WriteLine(now);
+            Console.ReadLine();
+            Console.WriteLine("Please enter a number");
+            int X = Convert.ToInt32(Console.ReadLine());
+
+        }
+    }
+}
---
+++ FileAssignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileIOApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            using (StreamWriter w = new Streamwriter(m_exePath + "\\" + "log.txt"))
+            {
+                file.WriteLine(number);
+            }
+        }
+    }
+}
