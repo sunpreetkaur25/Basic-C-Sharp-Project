ae4e394530bc15d6d41a1046cdcca40c8f680bf1
sunpreetkaur25 <Sunpreet.kaur2588@gmail.com>
2022-02-28 18:15:48 -0500

    assignments

--- Additional Features Assignment/Program.cs
+++ Additional Features Assignment/Program.cs
@@ -8,11 +8,20 @@ namespace Additional_Features_Assignment
     internal class Program
     {
         static void Main(string[] args)
-        { }
-            class C
         {
-            public const int var = 12;
+            const int var = 12;
+            var C = 14;
+        }
+        class C
+        {
+            public C() : this(10)
+            {
+            }
+
+            public C(int age)
+            {
+            }
         }
     }
-    }
+}
 
