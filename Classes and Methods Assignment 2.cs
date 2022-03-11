a653ae5769861d0b84c2a5ae9c455f43227fcfc1
sunpreetkaur25 <Sunpreet.kaur2588@gmail.com>
2022-03-10 19:12:36 -0500

    Classes and Methods Assignment 2

---
+++ Classes and Methods Assignment 2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instatiate class
+            Program p = new Program();
+            Console.WriteLine("Input two numbers:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The result:" + nums(num1, num2));
+
+        }
+
+        public static int nums(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
