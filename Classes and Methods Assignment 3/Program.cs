4246736a7e0c6396cac19584f0335be3c420ef13
sunpreetkaur25 <Sunpreet.kaur2588@gmail.com>
2022-03-10 19:18:34 -0500

    ClassesAssignment3

---
+++ Classes and Methods Assignment 3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Assignment3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter two numbers");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Result:" + some_ops(num1, num2));
+        }
+
+        public static int some_ops(int num1, int num2)
+        {
+            return num1 + num1;
+        }
+    }
+}
+
